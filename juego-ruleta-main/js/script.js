b9e37d52a7a625108fa6fec4f0b495ebea51b66e
Alejandro <alequinteroz75@gmail.com>
2022-07-22 22:21:30 -0600

    Cambios en algunos detalles

--- juego-ruleta-main/js/script.js
+++ juego-ruleta-main/js/script.js
@@ -1,65 +1,78 @@
 const ruleta = document.querySelector('#ruleta');
 
-ruleta.addEventListener('click', girar);
+var girando = true;
+
+
 giros = 0;
 
+const valores = ['Cafe 1820', 'Una Coca Cola de 2L', 'Arrocera', 'Frijoles Tio Pelon', 'Unas Galletas de Chocolate'];
 
-function girar(){
-  //if (giros < 3) {
+const column = document.querySelector('#columnaValue');
+//column.className = 'text-primary';
+
+
+for (let i = 0; i < valores.length; i++) {
+  const tr = document.createElement("tr");
+  tr.textContent = valores[i];
+  tr.id = valores[i].replace(/ /g, "");
+  //tr.className = 'text-primary';
+  column.appendChild(tr);
+}
+
+ruleta.addEventListener('click', girar);
+
+function girar() {
+  //si esta girando no hace nada
+  if (girando) {
+    girando = false;
+
     let rand = Math.random() * 7200;
     calcular(rand);
     giros++;
     var sonido = document.querySelector('#audio');
     sonido.setAttribute('src', 'sonido/ruleta.mp3');
-    document.querySelector('.contador').innerHTML = 'TURNOS: ' + giros; 
-  /*}else{
-    Swal.fire({
-      icon: 'success',
-      title: 'VUELVA PRONTO EL JUEGO TERMINO!!',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'Aceptar',
-      allowOutsideClick: false
-    }).then((result)=>{
-      if (result.value == true) {
-        giros = 0;
-         document.querySelector('.elije').innerHTML = 'TU CORTESIA ES: ';
-         document.querySelector('.contador').innerHTML = 'TURNOS: ' + giros;        
-      }
-    })
-  }
-*/
+    document.querySelector('.contador').textContent = 'TURNOS: ' + giros;
 
-function premio(premios){ 
+    function premio(premios) {
 
-  Swal.fire({
-    title: "-Premio-\n"+premios,
-    confirmButtonColor: '#3085d6',
-    confirmButtonText: 'Aceptar',
-    allowOutsideClick: false
-  }).then((result)=>{
-    if (result.value == true) {
-      giros = 0;
-       document.querySelector('.elije').innerHTML = 'Ultimo premio: '+ premios;
-       document.querySelector('.contador').innerHTML = 'TURNOS: ' + giros;        
+      Swal.fire({
+        title: "-Premio-\n" + premios,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Aceptar',
+        allowOutsideClick: false
+      }).then((result) => {
+        if (result.value == true) {
+          girando = true;
+          giros = 0;
+          document.querySelector('.elije').textContent = 'Ultimo premio: ' + premios; 
+
+        }
+      }) 
     }
-  })
 
-  document.querySelector('.elije').innerHTML = 'El ganador se lleva: ' + premios; 
-} 
+    function calcular(rand) {
 
- function calcular(rand) {
+      const valores = ['Cafe 1820', 'Una Coca Cola de 2L', 'Arrocera', 'Frijoles Tio Pelon', 'Unas Galletas de Chocolate'];
 
-  const valores = ['Cafe 1820', 'Una Coca Cola de 2L', 'Arrocera', 'Frijoles Tio Pelon', 'Unas Galletas de Chocolate' ];
+      valor = rand / 360;
+      valor = (valor - parseInt(valor.toString().split(".")[0])) * 360;
+      ruleta.style.transform = "rotate(" + rand + "deg)";
 
-  valor = rand / 360;
-  valor = (valor - parseInt(valor.toString().split(".")[0]))* 360;
-  ruleta.style.transform = "rotate("+rand+"deg)";
-
-  const random = Math.floor(Math.random() * valores.length);
+      const random = Math.floor(Math.random() * valores.length);
 
 
-  setTimeout(() => {
-    premio(valores[random]); }, 5000);
+      setTimeout(() => {
+        premio(valores[random]);
 
-}
+        //Una vez que salga el premio, se puede cambio el color de la letra
+        var ganado = document.querySelector(`#${valores[random].replace(/ /g, "")}`);
+        
+        ganado.className = 'texto-ganado'; 
+
+      }, 5000);
+
+    }
+  }
+
+
 }
